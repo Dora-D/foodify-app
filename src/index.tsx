ea5c36a3cc7476665884c4ebb8099dd7be81bc05
Dora <dan.zaychenko.2001@gmail.com>
2021-10-07 19:01:31 +0300

    component app added

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import { App } from "./components/App/App";
+
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>React App</h1>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
