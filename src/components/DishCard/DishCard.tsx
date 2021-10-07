c488c486e8b57827fa5106904796ea90df4f731b
Dora <dan.zaychenko.2001@gmail.com>
2021-10-07 21:03:08 +0300

    design for dish card added

--- src/components/DishCard/DishCard.tsx
+++ src/components/DishCard/DishCard.tsx
@@ -1,5 +1,25 @@
 import "./dish-card.scss";
 
 export const DishCard = () => {
-  return <div className="dish-card">Dish Card</div>;
+  return (
+    <div className="dish-card">
+      <img
+        src="https://lh3.googleusercontent.com/proxy/GT_uRGHMA-Ck15EQWEXnvnvhnAxwFJ8ODaFXxTmgszl3r2UYIKe_UMF0yZt5_fq36ENTAz0pDgJ6uHN31Xdf-rOWAt-Q8ambLz7Ad_9czHt4"
+        alt="Dish Img"
+      />
+      <div className="dish-card__info">
+        <h2>Titile</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+          veritatis sint debitis incidunt commodi? Nulla ducimus porro sed
+          deleniti accusamus perferendis quas, possimus libero adipisci
+          nesciunt, aliquid dolores, doloribus expedita quaerat! Odit illum
+          voluptates beatae fugit at in quidem ut a. Fuga facere natus earum
+          error ipsum quis ipsa iste! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Reiciendis, nostrum atque. Iste voluptates facere,
+          vitae dolores maiores unde illum libero.
+        </p>
+      </div>
+    </div>
+  );
 };
