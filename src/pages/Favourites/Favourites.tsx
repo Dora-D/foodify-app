6696463b0f970fe6d84ac826e969d9bf3adb9cc4
Dora <dan.zaychenko.2001@gmail.com>
2021-10-07 23:26:02 +0300

    logic to display modal added

--- src/pages/Favourites/Favourites.tsx
+++ src/pages/Favourites/Favourites.tsx
@@ -7,11 +7,16 @@ import "./favourites.scss";
 export const Favourites = () => {
   const [isModal, setIsModal] = useState<boolean>(false);
 
+  const portal = document.getElementById("portal") as HTMLElement;
+
+  const hendleModal = () => setIsModal((prevState) => !prevState);
+
   return (
     <div className="favourites">
-      <button className="favourites__add-recipe">
+      <button className="favourites__add-recipe" onClick={hendleModal}>
         <img src={plusIcon} alt="Plus Recipe" />
       </button>
+      {isModal && createPortal(<h1>Portal</h1>, portal)}
     </div>
   );
 };
