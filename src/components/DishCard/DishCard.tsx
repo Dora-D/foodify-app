46cf053e4c3a3501bb40ac3b8f8bded40a0cf225
Dora <dan.zaychenko.2001@gmail.com>
2021-10-08 01:52:38 +0300

    placeholder img to card dish added

--- src/components/DishCard/DishCard.tsx
+++ src/components/DishCard/DishCard.tsx
@@ -1,13 +1,14 @@
 import { FC } from "react";
 
 import { IDish } from "../../static";
+import pictureIcon from "../../assets/imgs/picture-icon.svg";
 import "./dish-card.scss";
 
 export const DishCard: FC<IDish> = ({ dishImg, title, recipe }) => {
   return (
     <div className="dish-card">
       <div className="dish-card__img-wrapper">
-        <img src={dishImg} alt="Dish Img" />
+        <img src={dishImg ? dishImg : pictureIcon} alt="Dish Img" />
       </div>
       <div className="dish-card__info">
         <h2>{title}</h2>
