6812c1b42a131b7a975d7406d7c47b4d34b19c05
Dora <dan.zaychenko.2001@gmail.com>
2021-10-07 23:20:56 +0300

    design for page favourites added

--- src/pages/Favourites/Favourites.tsx
+++ src/pages/Favourites/Favourites.tsx
@@ -1,3 +1,17 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
+
+import plusIcon from "../../assets/imgs/plus-icon.svg";
+import "./favourites.scss";
+
 export const Favourites = () => {
-  return <h1>Favorites</h1>;
+  const [isModal, setIsModal] = useState<boolean>(false);
+
+  return (
+    <div className="favourites">
+      <button className="favourites__add-recipe">
+        <img src={plusIcon} alt="Plus Recipe" />
+      </button>
+    </div>
+  );
 };
